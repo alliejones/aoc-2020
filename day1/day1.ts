f179fc9091c74735d9f351cfddd2bbcbfa5ebc02
Allie Jones <amjones@etsy.com>
2020-12-01 17:52:20 -0500

    Day 1 part 1

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,1 +1,19 @@
-console.log("hello, world");
+const INPUT_FILE = "./day1/input.txt";
+
+const vals = await Deno.readTextFile(INPUT_FILE).then((input) =>
+  input.split("\n").map(Number)
+);
+
+// IIFE to allow early return instead of the weird JS syntax for breaking from nested loops
+(function () {
+  for (let i = 0; i < vals.length; i++) {
+    for (let j = i + 1; j < vals.length; j++) {
+      if (vals[i] + vals[j] === 2020) {
+        console.log(vals[i] * vals[j]);
+        return;
+      }
+    }
+  }
+})();
+
+export {};
