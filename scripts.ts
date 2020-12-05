193ea807a2db282d085465100030238c55da887d
Allie Jones <allie@alliejon.es>
2020-12-04 22:49:16 -0500

    Update test script and dependencies

--- scripts.ts
+++ scripts.ts
@@ -3,7 +3,11 @@ import { expandGlobSync } from "https://deno.land/std@0.79.0/fs/expand_glob.ts";
 const scripts: { [key: string]: string } = {};
 for (const file of expandGlobSync("day*/*.ts")) {
   if (file.isFile) {
-    scripts[file.name.replace(".ts", "")] = file.path;
+    if (file.name.endsWith("test.ts")) {
+      scripts[file.name.replace(".ts", "")] = `deno test ${file.path}`;
+    } else {
+      scripts[file.name.replace(".ts", "")] = file.path;
+    }
   }
 }
 
